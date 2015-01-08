83b6e83c0b0f4f3e2ecd54fec651d4baace466ec
bladechapman <bechapm2@illinois.edu>
2015-01-08 02:11:20 -0800

    dynamic http

--- server_test.js
+++ server_test.js
@@ -2,36 +2,38 @@ var jsdom = require('jsdom');
 var http = require('http');
 var qs = require('querystring');
 http.createServer(function (req, res) {
+	var postData = {};
+
 	if(req.method == 'POST') {
 		var buffer = "";
 		req.on('data', function(data) {
 			buffer += data;
 			if (buffer.length > 1e6)
-				request.connection.destroy();
+				req.connection.destroy();
 		})
 		req.on('end', function() {
-			console.log(qs.parse(buffer));
+			postData = qs.parse(buffer);
+
+			jsdom.env(
+				postData.url,
+				["http://code.jquery.com/jquery.js"],
+				function (errors, window) {
+					var links = window.$("a");
+					console.log(links.length + ' links for ' + postData.url)
+
+					var buffer = ""
+					for(var i in links) {
+						if(links[i].href && links[i].href.indexOf("wikipedia.org") > -1) {
+							buffer += links[i].href + '\n'
+						}
+					}
+
+					res.writeHead(200, {'Content-Type': 'text/plain'});
+					res.end(buffer);
+				}
+			);
 		})
 	}
-
-	jsdom.env(
-		"http://wikipedia.org/",
-		["http://code.jquery.com/jquery.js"],
-		function (errors, window) {
-			var links = window.$("a");
-
-			var buffer = ""
-			for(var i in links) {
-				if(links[i].href && links[i].href.indexOf("wikipedia.org") > -1) {
-					buffer += links[i].href + '\n'
-				}
-			}
-
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			res.end(buffer);
-		}
-	);
-
 
 
 }).listen(1337, '127.0.0.1');
